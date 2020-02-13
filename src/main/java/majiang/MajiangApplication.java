6f38fa6de7e8476bde0723374af862be443e5e2c
unknown <you@example.com>
2020-02-13 16:50:50 +0800

    init

---
+++ src/main/java/majiang/MajiangApplication.java
@@ -0,0 +1,13 @@
+package majiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajiangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajiangApplication.class, args);
+    }
+
+}
